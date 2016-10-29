ebf07bcde5ae03f826ac6a748c208a2e9f81c1fc
Heinrich Schild <hcschild@gmail.com>
2016-10-29 02:37:43 +0200

    Added jquery to package and fixed the Issues in pull request #12

--- aggregators/rules/ipg.js
+++ aggregators/rules/ipg.js
@@ -7,38 +7,38 @@ class IPG {
     find(parameter, callback) {
         jsDOM.env(
             this.location + parameter,
-            ["http://code.jquery.com/jquery.js"],
             /**
              *
              * @param err
              * @param window
-             * @param window.$ jquery
-             * @param window.$.each jquery for each loop
-             * @param window.$.prop jquery property
              */
             function(err,window){
-                let resultLength = (window.$(".entry-header h1:first").text()).length+6;
-                const result = ["**"+window.$(".entry-header h1:first").text()+"**\n"];
-                window.$(".alert-info").remove();
-                window.$(".entry-content h2, .entry-content p, .entry-content li, .entry-content td").each(function(){
-                    let text = window.$(this).text()+"\n";
-                    if(window.$(this).prop("nodeName").toLocaleLowerCase()==="h2"){
+                const maxLength = 2000;
+                const $ = require("jquery")(window);
+                let result = ["**"+$(".entry-header h1:first").text()+"**\n"];
+                const penalty = $(".alert-warning:first");
+                if(penalty.length>0){
+                    result.push("*"+penalty.text().replace(/\r?\n|\r/g, ' ').trim()+"*\n");
+                    penalty.remove();
+                }
+                $(".alert-info").remove();
+                $(".entry-content").children("h2, p, li, td").each(function(){
+                    let text = $(this).text()+"\n";
+                    if($(this).prop("nodeName").toLocaleLowerCase()==="h2"){
                         text = "\n**"+text+"**";
                     }
-                    if(resultLength+text.length>2000){
+                    if(result.join("").length+text.length>maxLength){
                         callback(result.join("").replace(/\n\s*\n\s*\n/g, '\n\n'));
-                        resultLength = 0;
-                        result.length = 0;
+                        result = [];
                     }
                     result.push(text);
-                    resultLength+=text.length;
                 });
                 callback(result.join("").replace(/\n\s*\n\s*\n/g, '\n\n'))
             }
         )
     }
 
-    getContent(parameter, callback) {
+    getContent(command, parameter, callback) {
         if (parameter) {
             const parameters = parameter.trim().split(" ");
             this.find(parameters[0].replace(".","-"), callback);

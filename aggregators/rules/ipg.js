80279f92a70f7efdfb769936992566336556b7ac
Heinrich Schild <hcschild@gmail.com>
2016-10-29 02:37:43 +0200

    Added jsdom to package. Added first version of an judge rules resources parser that converts the IPG without annotations to text.

--- aggregators/rules/ipg.js
+++ aggregators/rules/ipg.js
@@ -1,17 +1,49 @@
+const jsDOM = require("jsdom");
 class IPG {
     constructor() {
-        this.Location = "http://blogs.magicjudges.org/rules/ipg/";
+        this.location = "http://blogs.magicjudges.org/rules/ipg";
     }
 
     find(parameter, callback) {
-        //todo
+        jsDOM.env(
+            this.location + parameter,
+            ["http://code.jquery.com/jquery.js"],
+            /**
+             *
+             * @param err
+             * @param window
+             * @param window.$ jquery
+             * @param window.$.each jquery for each loop
+             * @param window.$.prop jquery property
+             */
+            function(err,window){
+                let resultLength = (window.$(".entry-header h1:first").text()).length+6;
+                const result = ["**"+window.$(".entry-header h1:first").text()+"**\n"];
+                window.$(".alert-info").remove();
+                window.$(".entry-content h2, .entry-content p, .entry-content li, .entry-content td").each(function(){
+                    let text = window.$(this).text()+"\n";
+                    if(window.$(this).prop("nodeName").toLocaleLowerCase()==="h2"){
+                        text = "\n**"+text+"**";
+                    }
+                    if(resultLength+text.length>2000){
+                        callback(result.join("").replace(/\n\s*\n\s*\n/g, '\n\n'));
+                        resultLength = 0;
+                        result.length = 0;
+                    }
+                    result.push(text);
+                    resultLength+=text.length;
+                });
+                callback(result.join("").replace(/\n\s*\n\s*\n/g, '\n\n'))
+            }
+        )
     }
 
     getContent(parameter, callback) {
         if (parameter) {
-            this.find(parameter, callback());
+            const parameters = parameter.trim().split(" ");
+            this.find(parameters[0].replace(".","-"), callback);
         } else {
-            callback(this.Location);
+            callback("<"+this.location+">");
         }
 
     }

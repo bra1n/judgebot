2cb22bc6cdb2645fe998316c95ddc7797f8637da
Heinrich Schild <hcschild@gmail.com>
2016-11-01 23:35:19 +0100

    changed the website parser to cheerio

--- aggregators/rules/ipg.js
+++ aggregators/rules/ipg.js
@@ -1,43 +1,45 @@
-const jsDOM = require("jsdom");
-const jQuery = require("jquery");
+const request = require("request");
+const cheerio = require("cheerio");
 class IPG {
     constructor() {
         this.location = "http://blogs.magicjudges.org/rules/ipg";
+        this.maxLength = 2000;
     }
 
     find(parameter, callback) {
-        jsDOM.env(
-            this.location + parameter,
-            /**
-             *
-             * @param err
-             * @param window
-             */
-            (err,window)=>{
-                const $ = jQuery(window);
-                const maxLength = 2000;
-                $(window);
-                let result = ["**"+$(".entry-header h1:first").text()+"**\n"];
-                const penalty = $(".alert-warning:first");
-                if(penalty.length>0){
-                    result.push("*"+penalty.text().replace(/\r?\n|\r/g, ' ').trim()+"*\n");
-                    penalty.remove();
+        request({
+                url: this.location + parameter
+            },
+            (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    const $ = cheerio.load(body);
+                    let result = ["**"+$(".entry-header h1").first().text()+"**\n"];
+
+                    const penalty = $(".alert-warning").first();
+                    if(penalty.length>0){
+                        result.push("*"+penalty.text().replace(/\r?\n|\r/g, ' ').trim().replace(" ",": ")+"*\n");
+                        penalty.remove();
+                    }
+
+                    const article =  $("article");
+                    article.find(".entry-header, .page-navigation, em").remove();
+
+                    const that = this;
+                    article.find("h2, p, li, td, .card-header").each(function(){
+                        let text = $(this).text()+"\n";
+                        const nodeName = $(this).prop("nodeName").toLowerCase();
+                        if(nodeName==="h2" || nodeName==="div"){
+                            text = "\n**"+text+"**";
+                        }
+                        if(result.join("").length+text.length>that.maxLength){
+                            callback(result.join("").replace(/\n\s*\n\s*\n/g, '\n\n'));
+                            result = [];
+                        }
+                        result.push(text);
+                    });
+                    callback(result.join("").replace(/\n\s*\n\s*\n/g, '\n\n'));
                 }
-                $(".alert-info").remove();
-                $(".entry-content").children("h2, p, li, td").each(function(){
-                    let text = $(this).text()+"\n";
-                    if($(this).prop("nodeName").toLocaleLowerCase()==="h2"){
-                        text = "\n**"+text+"**";
-                    }
-                    if(result.join("").length+text.length>maxLength){
-                        callback(result.join("").replace(/\n\s*\n\s*\n/g, '\n\n'));
-                        result = [];
-                    }
-                    result.push(text);
-                });
-                callback(result.join("").replace(/\n\s*\n\s*\n/g, '\n\n'))
-            }
-        )
+            });
     }
 
     getContent(command, parameter, callback) {

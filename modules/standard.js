a2bcc80f6c21fe87f98b32a169fb93db5bae77c8
hcschild <hcschild@gmail.com>
2017-08-31 18:16:03 +0200

    Added caching to !standard and made the response message shorter.

--- modules/standard.js
+++ modules/standard.js
@@ -10,6 +10,7 @@ class Standard{
         this.cachedEmbed = null;
         this.cachedTime = null;
         this.cacheExpireTime = 24*60*60*1000; //day in milliseconds
+        this.loadList()
     }
 
     getCommands(){
@@ -25,7 +26,7 @@ class Standard{
         const groupedSetList = _.groupBy(removedFutureSetList,'rough_exit_date');
         let description = "";
         _.forEach(groupedSetList,(value,key)=>{
-           description += '*Rotates '+key+':*\n ```'+value.map(set=>set.name).join(' | ')+'```\n\n';
+           description += '*Rotates '+key+':*```'+value.map(set=>set.name).join(' | ')+'```\n';
         });
         const embed = new Discord.RichEmbed({
             title: "Currently in Standard",
@@ -37,21 +38,24 @@ class Standard{
         return embed;
     }
 
-    handleMessage(command, parameter, msg) {
-        /*if(this.cachedEmbed != null && this.cachedTime != null && new Date().getTime()-this.cachedTime<this.cacheExpireTime){
-            return msg.channel.send('',{embed: this.cachedEmbed});
-        }*/
+    loadList(){
         rp({url: this.api, json:true}).then(body=>{
-         return msg.channel.send('',{embed: this.generateEmbed(body)});
+            return this.generateEmbed(body);
         },err=>{
             log.error("Error getting Standard list",err.error.details);
-            return msg.channel.send('',{embed: new Discord.RichEmbed({
+            return new Discord.RichEmbed({
                 title: "Standard - Error",
                 description: "Couldn't create Standard list.",
                 color: 0xff0000
-            })});
+            });
         });
+    }
 
+    handleMessage(command, parameter, msg) {
+        if(this.cachedEmbed != null && this.cachedTime != null && new Date().getTime()-this.cachedTime<this.cacheExpireTime){
+            return msg.channel.send('',{embed: this.cachedEmbed});
+        }
+        return msg.channel.send('',{embed: this.loadList()});
     }
 }
 

a37dfbc1bb17cda217212088f666d5f2ae08d013
Michael Milton <ttmigueltt@gmail.com>
2020-04-22 20:10:13 +1000

    Correct win conditions

--- modules/hangman.js
+++ modules/hangman.js
@@ -1,12 +1,12 @@
-const rp = require("request-promise-native");
-const _ = require("lodash");
-const Discord = require("discord.js");
-const utils = require("../utils");
+const rp = require('request-promise-native');
+const _ = require('lodash');
+const Discord = require('discord.js');
+const utils = require('../utils');
 const log = utils.getLogger('hangman');
-const Card = require("./card");
+const Card = require('./card');
 const cardFetcher = new Card();
 
-const GAME_TIME = 3*60*1000; // minutes
+const GAME_TIME = 3 * 60 * 1000; // minutes
 
 class HangmanGame {
     constructor({
@@ -23,17 +23,18 @@ class HangmanGame {
         this.collector = collector;
         this.card = card;
         this.difficulty = difficulty;
-        
+
         this.letters = new Set();
         this.wrongGuesses = 0;
         this.done = false;
+        this.gameSuccess = false;
     }
 
     /**
      * Handle a user guessing a letter in the word
      * @param {String} char The character that was guessed
      */
-    handleLetter(char){
+    handleLetter(char) {
         // get emoji character (we only accept :regional_indicator_X: emojis)
         this.letters.add(char);
         this.checkDone();
@@ -45,19 +46,18 @@ class HangmanGame {
      * @param {String} guess
      * @param {Discord.Message} msg
      */
-    handleGuess(guess, msg){
+    handleGuess(guess, msg) {
         const correct = this.card.name.toLowerCase();
-        if (guess.includes(correct)){
+        if (guess.includes(correct)) {
             // If they're correct, pretend we guessed all the letters individually
-            this.updateEmbed(true);
-            this.collector.stop('finished');
-            this.setDone();
+            this.setDone(true);
+            this.updateEmbed();
         }
         else {
             this.wrongGuesses++;
-            msg.react('❎');
             this.checkDone();
-            this.updateEmbed(false);
+            msg.react('❎');
+            this.updateEmbed();
         }
     }
 
@@ -65,19 +65,23 @@ class HangmanGame {
      * Checks if the game should finish
      */
     checkDone() {
-        if ((!this.missing.length) || this.wrong > 6) {
-            this.setDone();
+        if (!this.missing.length) {
+            this.setDone(true);
+        }
+        if (this.wrong > 6) {
+            this.setDone(false);
         }
     }
 
     /**
      * Indicates that this game is finished
      */
-    setDone(){
+    setDone(correct = false) {
         this.done = true;
+        this.gameSuccess = correct;
         this.updateEmbed();
         this.collector.stop('finished');
-        
+
         // remove guild / author ID from running games
         delete this.gameList[this.id];
     }
@@ -86,7 +90,7 @@ class HangmanGame {
      * Using the stored parameters, updates the embed for the specified game
      * @param {Boolean} forceCorrect If true, force the game to be won
      */
-    updateEmbed(forceCorrect = false){
+    updateEmbed(forceCorrect = false) {
         this.message.edit('', {embed: this.generateEmbed(forceCorrect)});
     }
 
@@ -94,7 +98,7 @@ class HangmanGame {
      * An array of letters that have not yet been guessed
      * @returns {Array}
      */
-    get missing(){
+    get missing() {
         const letterArr = Array.from(this.letters.values());
         return _.difference(_.uniq(this.card.name.replace(/[^a-z]/ig, '').toLowerCase().split('')), letterArr);
     }
@@ -103,13 +107,13 @@ class HangmanGame {
      * The number of wrong guesses made in this game
      * @returns {number}
      */
-    get wrong(){
+    get wrong() {
         const letterArr = Array.from(this.letters.values());
-        
+
         // The total number of mistakes is the sum of the incorrect letters, and incorrect guesses
         return letterArr.filter(c => this.card.name.toLowerCase().indexOf(c) === -1).length + this.wrongGuesses;
     }
-    
+
     /**
      * Return a discord Embed derived from this game
      * @param {Boolean} forceCorrect If true, force the game to be won
@@ -136,8 +140,8 @@ class HangmanGame {
         const correctPercent = Math.round((1 - (wrong / (totalGuesses || 1))) * 100);
 
         // generate embed title
-        const title = this.card.name.replace(/[a-z]/ig, c => !this.letters.has(c.toLowerCase()) ? '⬚':c);
-        let description = "";
+        const title = this.card.name.replace(/[a-z]/ig, c => !this.letters.has(c.toLowerCase()) ? '⬚' : c);
+        let description = '';
         // hard is without mana cost
         if (this.difficulty !== 'hard') {
             description += cardFetcher.renderEmojis(this.card.mana_cost) + '\n';
@@ -150,10 +154,10 @@ class HangmanGame {
         description += '```\n' +
             '   ____     \n' +
             `  |    |    Missing: ${missing.length} letter(s)\n` +
-            `  |    ${wrong > 0 ? 'o':' '}    Guessed: ${letterArr.join("").toUpperCase()}\n` +
-            `  |   ${wrong > 2 ? '/':' '}${wrong > 1 ? '|':' '}${wrong > 3 ? '\\':' '}   Correct: ${correctPercent}%\n` +
-            `  |    ${wrong > 1 ? '|':' '}    \n` +
-            `  |   ${wrong > 4 ? '/':' '} ${wrong > 5 ? '\\':' '}   \n` +
+            `  |    ${wrong > 0 ? 'o' : ' '}    Guessed: ${letterArr.join('').toUpperCase()}\n` +
+            `  |   ${wrong > 2 ? '/' : ' '}${wrong > 1 ? '|' : ' '}${wrong > 3 ? '\\' : ' '}   Correct: ${correctPercent}%\n` +
+            `  |    ${wrong > 1 ? '|' : ' '}    \n` +
+            `  |   ${wrong > 4 ? '/' : ' '} ${wrong > 5 ? '\\' : ' '}   \n` +
             ' _|________\n```\n' +
             'Use :regional_indicator_a::regional_indicator_b::regional_indicator_c: ... :regional_indicator_z: ' +
             'reactions to pick letters.';
@@ -169,14 +173,15 @@ class HangmanGame {
         // game is over
         if (this.done) {
             embed.setTitle(this.card.name);
-            embed.setFooter(missing.length ? 'You failed to guess the card!':'You guessed the card!');
+            embed.setFooter(this.gameSuccess ? 'You guessed the card!' :'You failed to guess the card!');
             embed.setURL(this.card.scryfall_uri);
             if (this.card.layout === 'transform' && this.card.card_faces && this.card.card_faces[0].image_uris) {
                 embed.setImage(this.card.card_faces[0].image_uris.normal);
-            } else if(this.card.image_uris) {
+            }
+            else if (this.card.image_uris) {
                 embed.setImage(this.card.image_uris.normal);
             }
-            embed.setColor(missing.length ? 0xff0000 : 0x00ff00);
+            embed.setColor(this.gameSuccess ? 0x00ff00 : 0xff0000);
         }
 
         return embed;
@@ -191,20 +196,20 @@ class MtgHangman {
                 inline: false,
                 description: 'Start a game of hangman, where you have to guess the card name with reaction letters',
                 help: 'Selects a random Magic card, token or plane and shows you the ' +
-                'placeholders for each letter in its name. To guess a letter, add a reaction to the ' +
-                'Hangman-message of the bot. Reactions have to be selected among the regional indicators ' +
-                ':regional_indicator_a: to :regional_indicator_z:. You can also use `!hangman guess some card` ' +
-                'to guess the card outright, but guessing wrongly will be penalised the same as a wrong letter. ' +
-                'You can only have one active game of ' +
-                'Hangman per Discord server, but you can also start an additional one in a private query.\n\n' +
-                '**Difficulty**:\n' +
-                'With an optional parameter you can select the difficulty. Available are `easy`, `medium` ' +
-                'and `hard`. Default is medium. "Easy" includes the mana cost and type line, "Medium" includes ' +
-                'the mana cost and "Hard" doesn\'t include any hints.',
-                examples: ["!hangman", "!hangman easy", "!hangman guess yare"]
+                    'placeholders for each letter in its name. To guess a letter, add a reaction to the ' +
+                    'Hangman-message of the bot. Reactions have to be selected among the regional indicators ' +
+                    ':regional_indicator_a: to :regional_indicator_z:. You can also use `!hangman guess some card` ' +
+                    'to guess the card outright, but guessing wrongly will be penalised the same as a wrong letter. ' +
+                    'You can only have one active game of ' +
+                    'Hangman per Discord server, but you can also start an additional one in a private query.\n\n' +
+                    '**Difficulty**:\n' +
+                    'With an optional parameter you can select the difficulty. Available are `easy`, `medium` ' +
+                    'and `hard`. Default is medium. "Easy" includes the mana cost and type line, "Medium" includes ' +
+                    'the mana cost and "Hard" doesn\'t include any hints.',
+                examples: ['!hangman', '!hangman easy', '!hangman guess yare']
             }
         };
-        this.cardApi = "https://api.scryfall.com/cards/random";
+        this.cardApi = 'https://api.scryfall.com/cards/random';
         this.runningGames = {};
     }
 
@@ -215,34 +220,35 @@ class MtgHangman {
     // generate the embed card
 
     handleMessage(command, parameter, msg) {
-        const [first, ...rest] = parameter.toLowerCase().split(" ");
+        const [first, ...rest] = parameter.toLowerCase().split(' ');
 
         // check for already running games
         const id = msg.guild ? msg.guild.id : msg.author.id;
         if (id in this.runningGames) {
             const game = this.runningGames[id];
             // The user can !hangman guess Some Card Name
-            if (first === 'guess'){
+            if (first === 'guess') {
                 const guess = rest.join(' ').toLowerCase();
                 game.handleGuess(guess, msg);
             }
             else {
                 msg.channel.send('', {
                     embed: new Discord.MessageEmbed({
-                        title: "Error",
-                        description: "You can only start one hangman game every " + GAME_TIME / 60000 + " minutes.",
+                        title: 'Error',
+                        description: 'You can only start one hangman game every ' + GAME_TIME / 60000 + ' minutes.',
                         color: 0xff0000
                     })
                 });
             }
             return;
-        } else {
+        }
+        else {
             // Handle the case where we "!hangman guess" but no game has started
             if (first === 'guess') {
                 msg.channel.send('', {
                     embed: new Discord.MessageEmbed({
-                        title: "Error",
-                        description: "No hangman game is currently running in this server. Guess ignored.",
+                        title: 'Error',
+                        description: 'No hangman game is currently running in this server. Guess ignored.',
                         color: 0xff0000
                     })
                 });
@@ -275,20 +281,24 @@ class MtgHangman {
                     }).on('end', (collected, reason) => {
                         game.setDone();
                     });
-                    
+
                     // Update the game object with pertinent information
                     game.collector = collector;
-                }).catch(() => {});
+                }).catch(() => {
+                });
             }
         }, err => {
-            log.error("Error getting random card from API", err.error.details);
-            msg.channel.send('', {embed: new Discord.MessageEmbed({
-                title: "Error",
-                description: "Scryfall is currently offline and can't generate us a random card, please try again later.",
-                color: 0xff0000
-            })});
+            log.error('Error getting random card from API', err.error.details);
+            msg.channel.send('', {
+                embed: new Discord.MessageEmbed({
+                    title: 'Error',
+                    description: 'Scryfall is currently offline and can\'t generate us a random card, please try again later.',
+                    color: 0xff0000
+                })
+            });
             delete this.runningGames[id];
-        }).catch(() => {});
+        }).catch(() => {
+        });
     }
 }
 
